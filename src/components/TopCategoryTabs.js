5e43bc655588f21afad34774d9d67669b91390c1
Shashikant Panchal <shashikant.panchal@excelindia.com>
2024-09-14 13:49:32 +0530

    UI changes

--- src/components/TopCategoryTabs.js
+++ src/components/TopCategoryTabs.js
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   selectedTab: {
-    backgroundColor: '#007BFF',
+    backgroundColor: 'black',
   },
   tabText: {
     fontWeight: '600',
