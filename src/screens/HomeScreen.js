a4b931d44465ebbf4076cdd7c9f9ea9ef4ba791d
Shashikant Panchal <shashikant.panchal@excelindia.com>
2024-09-15 01:31:55 +0530

    updated

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,8 +6,10 @@ import {
   StyleSheet,
   Dimensions,
   FlatList,
+  TouchableOpacity,
 } from 'react-native';
-import { Data } from '../data/Data';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {Data} from '../data/Data';
 
 const {width: viewportWidth, height: viewportHeight} = Dimensions.get('window');
 
@@ -17,6 +19,13 @@ const HomeScreen = () => {
       <ImageBackground
         source={{uri: item.image}}
         style={styles.imageBackground}>
+        <TouchableOpacity
+          style={styles.backIconContainer}
+          onPress={() => {
+            console.log('Nitin');
+          }}>
+          <Icon name="arrow-back" size={30} color="#fff" />
+        </TouchableOpacity>
         <View style={styles.headlineContainer}>
           <Text style={styles.headline}>{item.headline}</Text>
         </View>
@@ -28,20 +37,21 @@ const HomeScreen = () => {
   );
 
   return (
-    <FlatList
-      data={Data}
-      renderItem={renderItem}
-      keyExtractor={item => item.id}
-      pagingEnabled
-      showsVerticalScrollIndicator={false}
-      style={styles.list}
-    />
+    <View style={styles.list}>
+      <FlatList
+        data={Data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        pagingEnabled
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   list: {
-    flex: 1,
+    height: viewportHeight,
   },
   container: {
     width: viewportWidth,
@@ -51,24 +61,31 @@ const styles = StyleSheet.create({
     width: viewportWidth,
     height: viewportHeight / 2,
     justifyContent: 'flex-end',
+    position: 'relative',
+  },
+  backIconContainer: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    zIndex: 1,
   },
   headlineContainer: {
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     padding: 15,
   },
   headline: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    fontSize: 26,
+    fontWeight: '700',
     color: '#fff',
   },
   content: {
-    padding: 15,
+    padding: 20,
     backgroundColor: '#fff',
     height: '50%',
   },
   description: {
-    fontSize: 16,
-    color: '#333',
+    fontSize: 18,
+    color: '#555',
   },
 });
 
