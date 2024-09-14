a4df84e8739f3793e028d8eb7d5ceef13381f66a
Shashikant Panchal <shashikant.panchal@excelindia.com>
2024-09-14 12:24:33 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import BottomTabNavigator from './src/navigation/BottomTabNavigator';
+
+const App = () => {
+  return <BottomTabNavigator />;
+};
+
+export default App;
---
+++ src/navigation/BottomTabNavigator.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import HomeScreen from '../screens/HomeScreen';
+import DiscoverScreen from '../screens/DiscoverScreen';
+import SavedScreen from '../screens/SavedScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {StyleSheet} from 'react-native';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({color}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Discover') {
+              iconName = 'compass';
+            } else if (route.name === 'Saved') {
+              iconName = 'bookmark';
+            } else if (route.name === 'Profile') {
+              iconName = 'account';
+            }
+
+            return <Icon name={iconName} size={35} color={color} />;
+          },
+          tabBarStyle: styles.tabBar,
+          tabBarShowLabel: false,
+          tabBarItemStyle: styles.tabBarItem,
+          headerShown: false,
+        })}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Discover" component={DiscoverScreen} />
+        <Tab.Screen name="Saved" component={SavedScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabBar: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
+    elevation: 0,
+    borderRadius: 35,
+    height: 80,
+    backgroundColor: '#ffffff',
+    ...shadow,
+  },
+  tabBarItem: {
+    borderRadius: 25,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+  },
+});
+
+const shadow = {
+  shadowColor: 'black',
+  shadowOffset: {width: 0, height: 10},
+  shadowOpacity: 0.5,
+  shadowRadius: 15,
+  elevation: 5,
+};
+
+export default BottomTabNavigator;
